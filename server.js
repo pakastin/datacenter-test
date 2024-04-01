8633aae675ddad2523e4f48b1161c36348bbff40
Juha Lindstedt <juha@pakastin.fi>
2024-04-01 21:38:24 +0300

    Add geojson and map endpoints

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import { cloudflare, hetzner, upcloud } from '@pakastin/datacenters';
 import express from 'express';
 import { distance, point } from '@turf/turf';
 
+const data = { cloudflare, hetzner, upcloud };
+
 const { NODE_PORT } = process.env;
 
 const app = express();
@@ -13,10 +15,81 @@ app.get('/', async (req, res, next) => {
 
   res.set('Cache-Control', 'private, no-store');
   res.send([
-    `Cloudflare node: ${cloudflare[cf].city}`,
+    `Cloudflare node you're connected to: ${cloudflare[cf].city}`,
     `Nearest Hetzner data center: ${nearestHetzner.city}, distance ${Math.round(nearestHetzner.distance)} km.`,
     `Nearest UpCloud data center: ${nearestUpCloud.city}, distance ${Math.round(nearestUpCloud.distance)} km.`
   ].join('<br>'));
+});
+
+['cloudflare', 'hetzner', 'upcloud'].forEach(company => {
+  app.get(`/${company}.geojson`, async (req, res, next) => {
+    res.set('Cache-Control', `public, max-age=${60 * 60}`);
+    res.send({
+      type: 'FeatureCollection',
+      features: Object.entries(data[company]).map(([code, datacenter]) => {
+        const { city, country, lat, lng } = datacenter;
+
+        return {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [lng, lat]
+          },
+          properties: {
+            code,
+            city,
+            country
+          }
+        };
+      })
+    });
+  });
+
+  app.get(`/${company}/map.html`, async (req, res, next) => {
+    res.set('Cache-Control', `public, max-age=${60}`);
+    res.send(`<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://flyk.com/maplibre-gl/maplibre-gl.css">
+    <style>
+      #map {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script src="https://flyk.com/maplibre-gl/maplibre-gl.js"></script>
+    <script>
+      (async () => {
+        const style = await fetch('https://flyk.com/style/style.json').then(res => res.json());
+        
+        style.sources['${company}'] = {
+          type: 'geojson',
+          data: '../${company}.geojson'
+        };
+
+        style.layers.push({
+          id: '${company}',
+          source: '${company}',
+          type: 'circle'
+        });
+
+        const map = new maplibregl.Map({
+          container: 'map',
+          style
+        });
+      })();
+    </script>
+  </body>
+</html>`);
+  });
 });
 
 app.listen(NODE_PORT, (err) => {
